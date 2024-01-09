fc7f24e086bb33ce8c82812bef0ba5cb2cadd417
Anita Caron <anitacaron@users.noreply.github.com>
2024-01-09 17:28:24 -0300

    query ubergraph

--- src/ubergraph2asct/utils/query_utils.py
+++ src/ubergraph2asct/utils/query_utils.py
@@ -3,6 +3,7 @@ from typing import Iterator
 from oaklib.implementations import UbergraphImplementation
 
 oi = UbergraphImplementation()
+converter = oi.converter
 
 QUERY = """
     VALUES ?subject {{
@@ -15,6 +16,7 @@ QUERY = """
         {property}
     }}
     ?subject ?property ?object .
+    # LIMIT
 """
 
 
@@ -27,14 +29,35 @@ def query_ubergraph(query) -> Iterator:
     return oi.query(query=query, prefixes=prefixes)
 
 
-def query_seed(seed, prop) -> Iterator:
+def query_seed(seed, prop) -> str:
     """
     Query Ubergraph for the seed terms and properties
     """
-    return query_ubergraph(
-        QUERY.format(
-            subject=" ".join(seed), object=" ".join(seed), property=" ".join(prop)
+    return extract_triples(
+        query_ubergraph(
+            QUERY.format(
+                subject=" ".join(seed), object=" ".join(seed), property=" ".join(prop)
+            )
         )
+    )
+
+
+def query_label(seed) -> str:
+    """
+    Query Ubergraph to retrieve labels for the terms in seed
+    """
+    query_l = """
+        VALUES ?subject {{
+            {subject}
+        }}
+        VALUES ?property {{
+            {property}
+        }}
+        ?subject ?property ?object .
+        # LIMIT
+    """
+    return extract_triples(
+        query_ubergraph(query_l.format(subject=" ".join(seed), property="rdfs:label"))
     )
 
 
@@ -56,3 +79,31 @@ def get_prefixes(text, prefix_map):
             prefixes.append(prefix)
 
     return prefixes
+
+
+def extract_triples(entry: Iterator) -> str:
+    """
+    Extract triples into NT format
+    """
+    return "\n".join(
+        [
+            f"{to_uri(r['subject'])} {to_uri(r['property'])} {to_uri(r['object'])} ."
+            for r in entry
+        ]
+    )
+
+
+def to_uri(curie: str) -> str:
+    """
+    Expand CURIE and add < > to the URI
+    If it's a string, return input
+    """
+    is_curie = False
+    expand = ""
+    try:
+        expand = converter.expand(curie)
+        is_curie = True
+    except ValueError:
+        expand = curie
+
+    return f"<{expand}>" if is_curie else f'"{expand}"'
